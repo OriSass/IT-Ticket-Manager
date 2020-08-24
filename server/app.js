79986c7c592ee5abfe17758624df9ddbdbc4c3e9
OriSass <67857592+OriSass@users.noreply.github.com>
2020-08-24 14:49:37 +0300

    get requirments fullfilled

--- server/app.js
+++ server/app.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const { json } = require('express');
 
+
+
+app.get('/api/tickets', (request, response) => {
+    const data = fs.readFileSync('data.json');
+    let tickets = JSON.parse(data);
+    let searchText = request.query.searchText;
+    if(searchText){
+        const resultTickets = tickets.filter((ticket) => {
+                    let titleLower = ticket["title"].toLowerCase();
+                    return titleLower.indexOf(searchText.toLowerCase()) !== -1
+        });
+        response.send(resultTickets);
+    }
+    response.send(tickets);
+});
+
+app.post('api/tickets/:ticketId/done', (request, response) =>{
+    const data = fs.readFileSync('data.json');
+    let tickets = JSON.parse(data);
+});
 
 module.exports = app;
